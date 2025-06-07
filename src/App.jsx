1c18c861bda0dcd487da4735c614cd39c8ac0430
roulaimad <roulaimad7@gmail.com>
2025-06-07 20:37:55 +0100

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ArduinoTutorialsComponent from './Components/ArduinoTutorialsComponent'
+
+function App() {
+
+  return (
+    <>
+     <ArduinoTutorialsComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/ArduinoTutorialsComponent.jsx
@@ -0,0 +1,11 @@
+import orangeBackground from "../assets/orange-background.svg"
+function ArduinoTutorialsComponent() {
+  return (
+    <div>
+        <img src={orangeBackground} ></img>
+      <h1>Arduino tutorials for beginners</h1>
+    </div>
+  )
+}
+
+export default ArduinoTutorialsComponent
