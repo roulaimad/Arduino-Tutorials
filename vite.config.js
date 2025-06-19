2cfa9af5375a27004be58c974d1822a99839e7de
imad <roulaimad7@gmail.com>
2025-06-19 02:28:51 +0100

    Final deploy setup for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/Arduino-Tutorials/',
-  plugins: [
-    tailwindcss(),
-  ],
-})
-
+  base: "/Arduino-Tutorials/",
+  plugins: [tailwindcss()],
+});
