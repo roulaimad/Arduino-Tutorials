8f39707e11e433d2bf466a8327e5cfd77de5d39f
imad <roulaimad7@gmail.com>
2025-06-19 02:22:22 +0100

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: '/Arduino-Tutorials/',
   plugins: [
     tailwindcss(),
   ],
 })
+
